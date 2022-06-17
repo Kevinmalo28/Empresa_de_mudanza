12cc63b9408f4c9281d90f90f8b4d0874c16c3bc
Kevin Macías <92650610+Kevinmalo28@users.noreply.github.com>
2022-06-16 22:19:28 -0500

    Add files via upload

---
+++ dsdsfs/Capast_Typescript/database/config.ts
@@ -0,0 +1,17 @@
+//Configracion de la base de datos para poder ejecutar sin probelmas al momento de requerirla.
+import { connect }  from 'mongoose';
+
+const dbConnection = async () =>{
+    try {
+        
+        await  connect(process.env["MONGODB_CNN"] || "")
+        console.log(`Base de datos ejecutándose sin problema`);
+    } catch (error) {
+        console.log(error);
+        throw new Error(`Base de datos no disponible`)        
+    }
+}
+
+export  {
+    dbConnection
+}
